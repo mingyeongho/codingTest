1a2e60e9c63df98530f625638b5134b5804498c9
민경호 <1736s@naver.com>
2025-05-22 12:01:54 +0900

    [Silver2] 11279 최대힙

---
+++ BOJ/Silver/11279_최대힙/solution.py
@@ -0,0 +1,13 @@
+import sys
+from heapq import heappush, heappop
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+heap = []
+for _ in range(n):
+  m = int(input().rstrip())
+  if m == 0:
+    print(0 if len(heap) == 0 else heappop(heap)[1])
+  else:
+    heappush(heap, (-1 * m, m))
