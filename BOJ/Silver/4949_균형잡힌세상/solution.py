3b5f0ae415ae501fe5bf586b30255feae366d2b2
민경호 <1736s@naver.com>
2025-06-10 13:49:59 +0900

    [Silver4] 4949 균형잡힌 세상

--- BOJ/Silver/4949_균형잡힌세상/solution.py
+++ BOJ/Silver/4949_균형잡힌세상/solution.py
@@ -1,27 +1,26 @@
 import sys
-
 input = sys.stdin.readline
 
 while True:
-  sentence = input().rstrip()
-  if sentence == '.':
-    break
-  brackets = []
-  for s in sentence:
-    if s == '(' or s == '[':
-      brackets.append(s)
-    elif s == ')':
-      if brackets and brackets[-1] == '(':
-        brackets.pop()
-      else:
-        print("no")
-        break
-    elif s == ']':
-      if brackets and brackets[-1] == '[':
-        brackets.pop()
-      else:
-        print("no")
-        break
-    elif s == '.':
-      print('yes' if len(brackets) == 0 else 'no')
-      break
+    s = input().rstrip()
+
+    if s == '.':
+        exit(0)
+
+    stack = []
+    enable = True
+
+    for w in s:
+        if w == '(' or w == '[':
+            stack.append(w)
+        elif w == ')':
+            if not stack or stack[-1] != '(':
+                enable = False
+                break
+            stack.pop()
+        elif w == ']':
+            if not stack or stack[-1] != '[':
+                enable = False
+                break
+            stack.pop()
+    print("yes" if not stack and enable else 'no')
