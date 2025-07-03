f9b40b1828fd141d4bb552b730bc9e58d25d9c37
민경호 <1736s@naver.com>
2025-07-03 15:45:33 +0900

    rm: previous resolved problem

--- BOJ/Bronze/2562_최댓값/solution.py
+++
@@ -1,7 +0,0 @@
-import sys
-input = sys.stdin.readline
-
-values = [int(input().strip()) for _ in range(9)] # 9개의 줄에 입력을 받을 수 있고 그 값을 values에 저장
-
-print(max(values))
-print(values.index(max(values)) + 1)
--- BOJ/Bronze/9093_단어뒤집기/solution.py
+++
@@ -1,9 +0,0 @@
-# 리스트 슬라이싱을 이용한 방법, Time: 72ms
-import sys
-input = sys.stdin.readline
-
-n = int(input().strip())
-
-for _ in range(n):
-    reversed = [word[::-1] for word in input().split()]
-    print(" ".join(reversed))
