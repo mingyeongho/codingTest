f2eea4de7e9b9228702f57eb32cc4b8c67c59bed
민경호 <1736s@naver.com>
2025-07-04 14:10:59 +0900

    [Silver2] 1780 종이의 개수

---
+++ BOJ/재귀/1780_종이의 개수/solution.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input().strip())
+graph = [list(map(int, input().split())) for _ in range(N)]
+
+count = [0, 0, 0]
+
+
+def recur(x, y, n):
+    num = graph[x][y]
+    same = True
+
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if graph[i][j] != num:
+                same = False
+        if not same:
+            break
+
+    if same:
+        count[num + 1] += 1
+    else:
+        new_n = n // 3
+        for i in [0, new_n, 2*new_n]:
+            for j in [0, new_n, 2*new_n]:
+                recur(x+i, y+j, new_n)
+
+
+recur(0, 0, N)
+print(*count, sep='\n')
