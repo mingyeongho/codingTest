6f8f03cd9d4af63c5294239511c7abb2fb56fff8
민경호 <1736s@naver.com>
2025-07-07 14:58:52 +0900

    [Silver3] 15657 N과 M (8)

---
+++ BOJ/백트래킹/15657_N과 M (8)/README.md
@@ -0,0 +1,11 @@
+Level: Silver3
+
+> [!question] Question
+> 고른 수열은 비내림차순이어야 한다.
+
+> [!success] How?
+> backtrack에 start 매개변수를 추가
+
+> [!info] Think
+
+> [!tip] Time Complexity
---
+++ BOJ/백트래킹/15657_N과 M (8)/solution.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+
+ans = [0] * M
+
+
+def backtrack(k, start):
+    if k == M:
+        print(*ans)
+        return
+    for i in range(start, N):
+        ans[k] = nums[i]
+        backtrack(k+1, i)
+
+
+backtrack(0, 0)
