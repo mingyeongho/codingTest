a1c23f5bb369b4f851391ebe9d61e893b8bb1523
민경호 <1736s@naver.com>
2025-07-07 18:32:42 +0900

    [Silver2] 15666 N과 M (12)

---
+++ BOJ/백트래킹/15666_N과 M (12)/README.md
@@ -0,0 +1,13 @@
+Level: Silver2
+
+> [!question] Question
+> N개의 자연수 중 M개를 고른 수열
+> 같은 수를 여러번 골라도 된다.
+> 골느 수열은 비내림차순이어야 한다.
+> 중복되는 수열을 여러 번 출력하면 안된다.
+
+> [!success] How?
+
+> [!info] Think
+
+> [!tip] Time Complexity
---
+++ BOJ/백트래킹/15666_N과 M (12)/solution.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+
+ans = [0] * M
+res = set()
+
+
+def backtrack(k, start):
+    if k == M:
+        result = ' '.join(map(str, ans))
+        res.add(result)
+        return
+    for i in range(start, N):
+        ans[k] = nums[i]
+        backtrack(k+1, i)
+
+
+backtrack(0, 0)
+
+res = sorted(res, key=lambda x: list(map(int, x.split())))
+for r in res:
+    print(r)
